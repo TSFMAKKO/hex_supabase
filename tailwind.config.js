dfb128e6eeffc7d9d92dfe6f616a34d5aa4676c9
tsfmakko <racingf1@gmail.com>
2025-10-09 18:28:52 +0800

    tailwind 成功安裝

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "tailwindcss";
+
+export default defineConfig({
+  theme: {
+    extend: {
+      colors: {
+        brand: "#1da1f2",
+      },
+    },
+  },
+  plugins: [],
+});
