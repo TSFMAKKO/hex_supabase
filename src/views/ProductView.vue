b29c8b7a09d08994396bde562cffc8fdf1056eb0
yuchin <racingf1@gmail.com>
2025-10-11 01:28:59 +0800

    調整底部細節

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -69,7 +69,7 @@
       </div>
 
       <div v-if="loading">載入中…</div>
-      <div v-else class="max-w-[1076px] mb-[80px]">
+      <div v-else class="max-w-[1076px] mb-[50px] max-sm:mb-[16px]">
         <!-- <div class="flex w-[33.3%]">aaa</div> -->
         <!-- <div v-if="loading">載入中…</div> -->
         <!-- <div v-else> -->
@@ -134,8 +134,14 @@
     </div>
   </div>
 
-  <div class="flex w-full justify-center items-center gap-x-[24px] h-[96px] bg-[#E8FEC5] mb-[64px] px-[24px]">
-    <div class="flex text-[24px] max-sm:text-[20px] font-[700] font-noto leading-[120%] tracking-[1.2px] max-sm:tracking-[1px]">加入會員取得 9 折優惠</div>
+  <div class="flex w-full justify-center items-center gap-x-[24px] h-[96px] 
+    max-sm:h-[88px] bg-[#E8FEC5] mb-[64px] px-[24px]
+    max-sm:mb-[32px]
+  ">
+    <div
+      class="flex text-[24px] max-sm:text-[20px] font-[700] font-noto leading-[120%] tracking-[1.2px] max-sm:tracking-[1px]
+      ">
+      加入會員取得 9 折優惠</div>
     <div class="">
       <a class="flex justify-center max-xs:text-[red] px-[16px] py-[8px] bg-[#212529] text-[#FFFFFF] rounded-[4px] hover:bg-[#495057] focus:bg-[#495057] xs:text-[10px]"
         href="">馬上註冊
@@ -145,14 +151,17 @@
 
   <div class="flex w-full px-5 ">
     <div
-      class="flex max-sm:items-start max-sm:flex-col h-[48px] items-center justify-between w-[1296px] m-0 mx-auto max-sm:gap-y-[8px] ">
-      <div class="text-[28px] font-[700] leading-[120%] tracking-[1.4px] py-[8px] px-[12px]">
+      class="flex max-sm:items-start max-sm:flex-col h-[48px] items-center justify-between w-[1296px]
+       m-0 mx-auto  max-sm:mb-[132px] mb-[24px]">
+      <div class="text-[28px] font-[700] leading-[120%] tracking-[1.4px] py-[8px] max-sm:mb-[8px]">
         <img src="../assets/logo.svg" alt="" srcset="">
       </div>
       <div
-        class="flex max-sm:flex-col gap-x-[40px] max-sm:gap-y-[4px] text-[16px] font-[500] leading-[150%] tracking-[0.8px] px-[12px]">
-        <div class="w-full text-left">商品列表</div>
-        <div class="w-full text-left mb-[24px] ">品牌故事</div>
+        class="flex max-sm:flex-col gap-x-[40px] max-sm:gap-y-[4px] text-[16px] 
+            font-[500] leading-[150%] 
+            tracking-[0.8px] px-[12px]">
+        <div class="w-full text-left py-[12px]">商品列表</div>
+        <div class="w-full text-left py-[12px]">品牌故事</div>
       </div>
     </div>
   </div>
