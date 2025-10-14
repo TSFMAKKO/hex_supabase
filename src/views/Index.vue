737ffefe50f74dcb2216406a916f5c5db5a1d530
yuchin <racingf1@gmail.com>
2025-10-14 17:24:07 +0800

    首頁大致完成

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -3,32 +3,67 @@
     <h1 class="mx-auto">Index Page</h1>
   </div> -->
 
-  <div class="benner w-[calc(100vw-15px)] -mx-5 overflow-hidden">
+  <div class="hidden max-sm:flex w-[calc(100vw-7px)] -mx-5">
+    <div class="h-[48px] w-[50%] text-[#212529] bg-[#E8FEC5] flex justify-center items-center">商品列表</div>
+    <div class="h-[48px] w-[50%] text-[#212529] bg-[#F5FEE7] flex justify-center items-center">品牌故事</div>
+  </div>
+
+  <div class="benner max-sm:hidden w-[calc(100vw-7px)] -mx-5 overflow-hidden mb-[64px] max-sm:mb-[40px]">
     <img class="w-full max-w-none  " src="../assets/banner.png" alt="">
   </div>
 
-  <div class="grid gap-y-[32px] max-w-[1296px] w-[90%] mx-auto">
+   <div class="benner sm:hidden w-[calc(100vw-7px)] -mx-5 overflow-hidden mb-[64px] max-sm:mb-[40px]">
+    <img class="w-full max-w-none  " src="../assets/banner-sm.png" alt="">
+  </div>
+
+  <div class="grid gap-y-[32px] max-w-[1296px] w-[90%] mx-auto py-[64px] max-sm:py-[40px]">
     <div>
-      <div>New Arrival</div>
-      <div>新品上市</div>
+      <div class="font-[700] text-[48px] leading-[1.2] tracking-[2.4px] text-center">New Arrival</div>
+      <div class="font-[700] text-[32px] leading-[1.2] tracking-[1.6px] text-center">新品上市</div>
     </div>
 
     <div class=" grid grid-cols-2 grid-rows-2 gap-y-[40px] gap-x-[24px]">
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
+      <div class="grid gap-y-[12px]">
+        <div><img src="../assets/new-1.png" alt="" srcset=""></div>
+        <div class="grid gap-y-[4px]">
+          <div class="font-[700] text-[20px] leading-[1.2] tracking-[1px]">Monoverse</div>
+          <div class="">NT$3,600</div>
+        </div>
+      </div>
+
+      <div class="grid gap-y-[12px]">
+        <div><img src="../assets/new-2.png" alt="" srcset=""></div>
+        <div class="grid gap-y-[4px]">
+          <div class="font-[700] text-[20px] leading-[1.2] tracking-[1px]">PLATFORM 404</div>
+          <div class="">NT$2,600</div>
+        </div>
+      </div>
+
+      <div class="grid gap-y-[12px]">
+        <div><img src="../assets/new-3.png" alt="" srcset=""></div>
+        <div class="grid gap-y-[4px]">
+          <div class="font-[700] text-[20px] leading-[1.2] tracking-[1px]">URBN Force</div>
+          <div class="">NT$3,200</div>
+        </div>
+      </div>
+
+      <div class="grid gap-y-[12px]">
+        <div><img src="../assets/new-4.png" alt="" srcset=""></div>
+        <div class="grid gap-y-[4px]">
+          <div class="font-[700] text-[20px] leading-[1.2] tracking-[1px]">Mode90</div>
+          <div class="">NT$4,000</div>
+        </div>
+      </div>
     </div>
   </div>
 
   <!-- 熱銷補貨 -->
-  <div class="flex flex-col gap-y-[16px] max-w-[1296px] w-[90%] max-sm:w-[100%] mx-auto">
+  <div class="flex flex-col gap-y-[16px] max-w-[1296px] w-[90%] max-sm:w-[100%] mx-auto py-[64px] max-sm:py-[40px]">
     <div class="font-[700] text-[32px] leading-[1.2] tracking-[1.6px]">熱銷補貨</div>
 
     <!-- 卷軸容器（桌面：grid；小螢幕：水平滑動） -->
     <div class="overflow-x-auto">
-      <div
-        class="grid grid-cols-4 gap-x-[24px] w-full max-sm:flex max-sm:gap-x-[16px]">
+      <div class="grid grid-cols-4 gap-x-[24px] w-full max-sm:flex max-sm:gap-x-[16px]">
         <div class="max-sm:w-[300px] max-sm:h-[300px] max-sm:shrink-0">
           <img class="w-full h-full object-cover" src="../assets/product-10.png" alt="">
         </div>
@@ -47,18 +82,21 @@
   </div>
 
   <!--  -->
-  <div class="w-[calc(100vw-15px)] -mx-5 overflow-hidden">
+  <div class="w-[calc(100vw-7px)] -mx-5 overflow-hidden max-sm:hidden  py-[64px] max-sm:py-[40px]">
     <img class="w-full max-w-none  " src="../assets/banner-co-branded.png" alt="">
   </div>
 
-    <!-- 依系列選購 -->
-  <div class="flex flex-col gap-y-[16px] max-w-[1296px] w-[90%] max-sm:w-[100%] mx-auto">
+    <div class="w-[calc(100vw-7px)] -mx-5 overflow-hidden py-[48px] max-sm:py-[24px] sm:hidden">
+    <img class="w-full max-w-none  " src="../assets/banner-co-branded-sm.png" alt="">
+  </div>
+
+  <!-- 依系列選購 -->
+  <div class="flex flex-col gap-y-[16px] max-w-[1296px] w-[90%] max-sm:w-[100%] mx-auto py-[64px] max-sm:py-[40px]">
     <div class="font-[700] text-[32px] leading-[1.2] tracking-[1.6px]">依系列選購</div>
     <!-- 卷軸容器（桌面：grid；小螢幕：水平滑動） -->
     <div class="overflow-x-auto">
-      <div
-        class="grid grid-cols-4 max-sm:grid-cols-2 gap-x-[24px] w-full max-sm:gap-x-[12px] max-sm:gap-y-[12px]">
-        
+      <div class="grid grid-cols-4 max-sm:grid-cols-2 gap-x-[24px] w-full max-sm:gap-x-[12px] max-sm:gap-y-[12px]">
+
         <div class="">
           <img class="w-full h-full object-cover" src="../assets/product-10.png" alt="">
         </div>
@@ -77,14 +115,14 @@
   </div>
 
   <!-- 穿搭靈感 -->
-  <div class="flex flex-col gap-y-[16px] max-w-[1296px] w-[90%] max-sm:w-[100%] mx-auto">
+  <div class="flex flex-col gap-y-[16px] max-w-[1296px] w-[90%] max-sm:w-[100%] mx-auto py-[64px] max-sm:py-[40px]">
     <div class="font-[700] text-[32px] leading-[1.2] tracking-[1.6px]">穿搭靈感</div>
 
     <!-- 卷軸容器 -->
     <div class="overflow-x-auto">
       <!-- grid 寬度固定，讓內容可橫向滾動 -->
       <div
-        class="grid grid-cols-6 gap-[8px] min-w-[1296px] max-sm:w-[calc((300px*12)+(16px*11))] max-sm:grid-cols-12 max-sm:gap-[16px]">
+        class="grid grid-cols-6 gap-[8px] max-w-[1296px] w-full max-sm:min-w-[calc((300px*12)+(16px*11))] max-sm:grid-cols-12 max-sm:gap-[16px]">
         <div class=""><img class="w-full" src="../assets/inspiration-1.png" alt=""></div>
         <div class=""><img class="w-full" src="../assets/inspiration-2.png" alt=""></div>
         <div><img class="w-full" src="../assets/inspiration-3.png" alt=""></div>
